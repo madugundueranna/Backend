40c8c9d75893222bb78b7d3459f565e699f1da20
madugundueranna <meranna60@gmail.com>
2026-06-05 17:17:23 +0530

    url change

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 1998;
 const job = require("./Config/Cron");
 const corsOptions = {
-  origin: process.env.WEB_URL || "http://localhost:5174",
+  origin: process.env.WEB_URL || "https://eventmangement2.netlify.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
